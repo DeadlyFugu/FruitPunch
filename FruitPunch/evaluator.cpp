6b11a7d54a0f8fb9f6d06d36e96b57a12b9aee37
Matthew Turner <deadlyfugu@gmail.com>
2016-03-28 10:38:22 +1100

    implements GETV, SETV, and BINDV opcodes

--- FruitPunch/evaluator.cpp
+++ FruitPunch/evaluator.cpp
@@ -6,24 +6,53 @@ void evaluate(Closure* closure, context_t root, Stack* stack) {
 	uint8_t* pc = closure->bytecode;
 	uint8_t* end = closure->bytecode + closure->len;
 
+	context_t locals = new_context();
+
 #define PUSH(obj) stack->push(obj)
+#define READ_PC_AS(type) *reinterpret_cast<type*>(pc)
+#define POP() stack->pop()
 	
 	while (pc < end) {
 		switch (*pc++) {
 		case 0x02:
 		{
-			PUSH(new_int32(*reinterpret_cast<uint32_t*>(pc)));
+			PUSH(new_int32(READ_PC_AS(uint32_t)));
 			pc += 4;
 		} break;
 		case 0x04:
 		{
-			PUSH(new_float32(*reinterpret_cast<float*>(pc)));
+			PUSH(new_float32(READ_PC_AS(float)));
+			pc += 4;
 		} break;
 		case 0x0B:
 		{
 			PUSH(new_string((char*) pc));
 			pc += strlen((char*) pc);
-		}
+		} break;
+		case 0x10:
+		{
+			// TODO callv
+		} break;
+		case 0x11:
+		{
+			// GETV
+			symbol_t sym = READ_PC_AS(symbol_t);
+			PUSH(locals->getv(sym));
+		} break;
+		case 0x12:
+		{
+			// SETV
+			symbol_t sym = READ_PC_AS(symbol_t);
+			obj_t value = POP();
+			locals->setv(sym, value);
+		} break;
+		case 0x13:
+		{
+			// BINDV
+			symbol_t sym = READ_PC_AS(symbol_t);
+			obj_t value = POP();
+			locals->bindv(sym, value);
+		} break;
 		}
 	}
 }
